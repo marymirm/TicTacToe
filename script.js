bc5cdc6ec66ca797fc1d15cde8b742b59b97bf5e
Ymir Meijles <ymirmar@live.com>
2022-11-30 14:24:04 +0100

    Make boilerplate and basic CSS

---
+++ script.js
@@ -0,0 +1,17 @@
+//module
+const GameBoard = (() => {
+    const GameBoardArray = ['1','2','3','4','5','6','7','8','9'];
+    
+    const getGameBoardArray = () => {
+        return GameBoardArray
+    }
+
+    console.log(getGameBoardArray);
+
+})();
+
+//factory function
+const player = (name) => {
+    const example = () => console.log(example);
+}
+
