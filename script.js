d1254c61cb544c9212edc7c677c4386fe656b39e
Ymir Meijles <ymirmar@live.com>
2022-11-30 15:48:23 +0100

    Render the contents of gameboard array to the webpage, be it manually

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 //module
 const GameBoard = (() => {
-    const GameBoardArray = ['1','2','3','4','5','6','7','8','9'];
-    
-    const getGameBoardArray = () => {
-        return GameBoardArray
-    }
+    const GameBoardArray = ['X'];
+    return {GameBoardArray};
+})();
+GameBoard.GameBoardArray;
 
-    console.log(getGameBoardArray);
-
+const game = (() => {
+    const box = document.querySelectorAll('.Box');
+    box[0].textContent = GameBoard.GameBoardArray;
 })();
 
 //factory function
-const player = (name) => {
-    const example = () => console.log(example);
-}
+const createPlayer = (playerName, playerNumber, XorO) => {
+    const getPlayerName = () => playerName;
+    
+    console.log("This is the name of Player" + ' ' + playerNumber + ':' + ' ' + playerName);
+    
+    return {getPlayerName, playerName, playerNumber, XorO}
+};
+
+let player1 = createPlayer("Netanjoehoe", 1, "X");
+let player2 = createPlayer("Andjella Miracle", 2, "O")
+
 
